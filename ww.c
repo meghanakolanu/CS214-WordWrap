cc1c792608ee2a3f5af20ecb7b4f42c3ec0a7a55
meghanakolanu <99992251+meghanakolanu@users.noreply.github.com>
2022-03-23 18:51:46 -0400

    Update ww.c

--- ww.c
+++ ww.c
@@ -9,8 +9,24 @@ inline int word_length(const char *str){
     return(getIndex);
 }
 
-void word_wrap(char *s, const int linewrap){
-    int index = 0;
+void word_wrap(char *s, const int newline){
+    int length;
+    int w;
+    int word_length = 0;
+    int ouput_length = 0;
+    int line_position = 0;
+
+    for(length = 0; str[length]; length++)
+
+    char word[length];
+
+    for(w = 0; w<=length; n++){
+        if(str[w] == '\n' || str[w] == '' || w = length ){
+            if(line_position > newline){
+
+            }
+        }
+    }
     
 
 }
