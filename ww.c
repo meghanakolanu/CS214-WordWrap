c433b8c2aac6e015991dd193f6f4bd0223a68c16
meghanakolanu <99992251+meghanakolanu@users.noreply.github.com>
2022-03-23 18:41:59 -0400

    Update ww.c

--- ww.c
+++ ww.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+inline int word_length(const char *str){
+    int getIndex = 0;
+    while(str[getIndex]! = 0 && str[getIndex] ! = '' && str[getIndex]! = '\n'){
+        getIndex++;
+    }
+    return(getIndex);
+}
+
+void word_wrap(char *s, const int linewrap){
+    int index = 0;
+    
+
+}
